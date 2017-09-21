a86f6b3aff23b516322317fe3a3e3b34fd5faed5
RuipengWang <rpwang@Edwards-MacBook-Pro.local>
2017-09-21 14:51:11 +0800

    Change code to deploy to test net -- Ruipeng Wang

--- truffle.js
+++ truffle.js
@@ -1,8 +1,8 @@
 // Following is used for Production Release, uncomment when deployment
-// var WalletProvider = require("truffle-wallet-provider");
-// var keystore = require('fs').readFileSync('./keystore').toString();
-// var pass = require('fs').readFileSync('./pass').toString();
-// var wallet = require('ethereumjs-wallet').fromV3(keystore, pass);
+var WalletProvider = require("truffle-wallet-provider");
+var keystore = require('fs').readFileSync('./keystore').toString();
+var pass = require('fs').readFileSync('./pass').toString();
+var wallet = require('ethereumjs-wallet').fromV3(keystore, pass);
 
 module.exports = {
     networks: {
@@ -15,5 +15,9 @@ module.exports = {
         //     network_id: 1,
         //     provider: new WalletProvider(wallet, "https://etc.crypedit.com"),
         // },
+        ropsten: {
+            network_id: 1,
+            provider: new WalletProvider(wallet, "http://45.76.197.72:8545"),
+        }
     }
 };
